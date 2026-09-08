bcc86635aeb381b196399d670e12ca797936b914
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-08 13:50:22 +0530

    Solution

--- Day-02 ( Swap Two Numbers Using a Temporary Variable ) /CPP/Solution.cpp
+++ Day-02 ( Swap Two Numbers Using a Temporary Variable ) /CPP/Solution.cpp
@@ -1,3 +1,6 @@
+
+// For Both a And b In Same Line.    
+
 #include <iostream>
 using namespace std;
 
@@ -17,3 +20,37 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+// For Both a And b In Different Line.  
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+
+    // Swap using temporary variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swap: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
+
+
+
--- Day-02 ( Swap Two Numbers Using a Temporary Variable ) /Python/Solution.py
+++ Day-02 ( Swap Two Numbers Using a Temporary Variable ) /Python/Solution.py
@@ -1,10 +1,43 @@
+
+# For Both a And b in Same Line. 
+# a, b = map(int, input().split()) 
+# expects both numbers in one line. 
+
+
 a, b = map(int, input().split())
 
 print(f"Before swap: a = {a}, b = {b}")
 
-# Swap using temporary variable
 temp = a
 a = b
 b = temp
 
 print(f"After swap: a = {a}, b = {b}")
+
+
+
+
+
+
+
+
+
+
+
+
+# For Both a And b In Different Line. 
+
+a = int(input())
+b = int(input())
+
+print(f"Before swap: a = {a}, b = {b}")
+
+temp = a
+a = b
+b = temp
+
+print(f"After swap: a = {a}, b = {b}")
+
+
+
+
